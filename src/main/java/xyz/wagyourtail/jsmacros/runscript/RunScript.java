85ea78e47b29ecebc453340f2879e6868be19985
wagyourtail <ruwagray@gmail.com>
2020-06-23 01:29:17 -0600

    use graal contexts and stop using scriptengine entirely.

--- src/main/java/xyz/wagyourtail/jsmacros/runscript/RunScript.java
+++ src/main/java/xyz/wagyourtail/jsmacros/runscript/RunScript.java
@@ -1,18 +1,14 @@
 package xyz.wagyourtail.jsmacros.runscript;
 
 import java.io.File;
-import java.io.FileReader;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import javax.script.ScriptEngine;
-
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Context.Builder;
 import org.graalvm.polyglot.HostAccess;
-
-import com.oracle.truffle.js.scriptengine.GraalJSScriptEngine;
+import org.graalvm.polyglot.Value;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.LiteralText;
@@ -29,6 +25,7 @@ import xyz.wagyourtail.jsmacros.runscript.functions.worldFunctions;
 
 public class RunScript {
     public static HashMap<RawMacro, ArrayList<Thread>> threads = new HashMap<>();
+    public static String language = "js";
     
     
     public static Thread exec(RawMacro macro, String event, HashMap<String, Object> args) {
@@ -39,25 +36,29 @@ public class RunScript {
             try {
                 File file = new File(jsMacros.config.macroFolder, macro.scriptFile);
                 if (file.exists()) {
-                    Builder context = Context.newBuilder("js");
-                    context.allowHostAccess(HostAccess.ALL);
-                    context.allowHostClassLookup(s -> true);
-                    context.allowAllAccess(true);
-                    context.allowExperimentalOptions(true);
-                    context.currentWorkingDirectory(Paths.get(file.getParent()));
-                    ScriptEngine engine = GraalJSScriptEngine.create(null, context);
-                    engine.put("event", event);
-                    engine.put("args", args);
-                    engine.put("file", file);
-                    engine.put("global", new globalVarFunctions());
-                    engine.put("jsmacros", new jsMacrosFunctions());
-                    engine.put("time", new timeFunctions());
-                    engine.put("keybind", new keybindFunctions());
-                    engine.put("chat", new chatFunctions());
-                    engine.put("world", new worldFunctions());
-                    engine.put("player", new playerFunctions());
-                    engine.put("hud", new hudFunctions());
-                    engine.eval(new FileReader(file));
+                    Builder build = Context.newBuilder(language);
+                    build.allowHostAccess(HostAccess.ALL);
+                    build.allowHostClassLookup(s -> true);
+                    build.allowAllAccess(true);
+                    build.allowExperimentalOptions(true);
+                    build.currentWorkingDirectory(Paths.get(file.getParent()));
+                    Context con = build.build();
+                    Value binds = con.getBindings(language);
+//                    ScriptEngine engine = GraalJSScriptEngine.create(null, build);
+                    binds.putMember("event", event);
+                    binds.putMember("args", args);
+                    binds.putMember("file", file);
+                    binds.putMember("global", new globalVarFunctions());
+                    binds.putMember("jsmacros", new jsMacrosFunctions());
+                    binds.putMember("time", new timeFunctions());
+                    binds.putMember("keybind", new keybindFunctions());
+                    binds.putMember("chat", new chatFunctions());
+                    binds.putMember("world", new worldFunctions());
+                    binds.putMember("player", new playerFunctions());
+                    binds.putMember("hud", new hudFunctions());
+                    con.eval(language, "load(\"./"+file.getName()+"\")");
+                    con.close();
+//                    engine.eval(new FileReader(file));
                 }
             } catch (Exception e) {
                 MinecraftClient mc = MinecraftClient.getInstance();
