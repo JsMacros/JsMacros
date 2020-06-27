f6458875c98058fb052cabf915cba1df2a8a93b2
wagyourtail <ruwagray@gmail.com>
2020-06-27 08:25:20 -0600

    add ways to make texthelpers easily from js.

--- src/main/java/xyz/wagyourtail/jsmacros/runscript/functions/chatFunctions.java
+++ src/main/java/xyz/wagyourtail/jsmacros/runscript/functions/chatFunctions.java
@@ -2,6 +2,7 @@ package xyz.wagyourtail.jsmacros.runscript.functions;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.LiteralText;
+import xyz.wagyourtail.jsmacros.reflector.TextHelper;
 
 public class chatFunctions {
     public void log(String message) {
@@ -12,10 +13,23 @@ public class chatFunctions {
         }
     }
     
+    public void log(TextHelper text) {
+        MinecraftClient mc = MinecraftClient.getInstance();
+        mc.inGameHud.getChatHud().addMessage(text.getRaw(), 0);
+    }
+    
     public void say(String message) {
         if (message != null) {
             MinecraftClient mc = MinecraftClient.getInstance();
             mc.player.sendChatMessage(message);
         }
+    }
+    
+    public TextHelper createTextHelperFromString(String content) {
+        return new TextHelper(new LiteralText(content));
+    }
+    
+    public TextHelper createTextHelperFromJSON(String json) {
+        return new TextHelper(json);
     }
 }
