9f12d064f43e6dd455ed85062eff9f4f2f64abe1
wagyourtail <ruwagray@gmail.com>
2020-06-29 10:39:00 -0600

    bump version, some work on TextInput.

--- src/main/java/xyz/wagyourtail/jsmacros/gui2/elements/TextInput.java
+++ src/main/java/xyz/wagyourtail/jsmacros/gui2/elements/TextInput.java
@@ -8,19 +8,44 @@ import net.minecraft.text.LiteralText;
 public class TextInput extends Button {
     public Consumer<String> onChange;
     public String content;
-    public int selStart;
-    public int selEnd;
+    protected int selStart;
+    public int selStartIndex;
+    protected int selEnd;
+    public int selEndIndex;
     
     public TextInput(int x, int y, int width, int height, int color, int borderColor, int hilightColor, int textColor, String message, Consumer<String> onChange) {
-        super(x, y, width, height, color, borderColor, hilightColor, textColor, new LiteralText(message), null);
+        super(x, y, width, height, color, borderColor, hilightColor, textColor, new LiteralText(""), null);
+        this.content = message;
         this.onChange = onChange;
+        this.updateSelStart(0);
+        this.updateSelEnd(0);
     }
     
     public void setMessage(String message) {
-        super.setMessage(new LiteralText(message));
+        content = message;
+    }
+    
+    public void updateSelStart(int startIndex) {
+        selStartIndex = startIndex;
+        if (startIndex == 0) selStart = x + 2;
+        else selStart = x + 2 + mc.textRenderer.getWidth(content.substring(0, startIndex));
+    }
+    
+    public void updateSelEnd(int endIndex) {
+        selEndIndex = endIndex;
+        if (endIndex == 0) selEnd = x + 3;
+        else selEnd = x + 3 + mc.textRenderer.getWidth(content.substring(0, endIndex));
+    }
+    
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        int pos = mc.textRenderer.trimToWidth(content, (int) (mouseX-x-2)).length();
+        updateSelStart(pos);
+        updateSelEnd(pos);
+        return super.mouseClicked(mouseX, mouseY, button);
     }
     
     protected void renderMessage(MatrixStack matricies) {
-        drawTextWithShadow(matricies, mc.textRenderer, text.get(0), x + 2, y + 2, textColor);
+        fill(matricies, selStart, y+2, selEnd, y+3+mc.textRenderer.fontHeight, hilightColor);
+        drawStringWithShadow(matricies, mc.textRenderer, content, x + 2, height > 9 ? y + 2 : y, this.isFocused() ? textColor : 0xFFFF0000);
     }
 }
