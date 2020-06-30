978250f4f2745ef7c81de2884ac8e44a476ebe6e
wagyourtail <ruwagray@gmail.com>
2020-06-30 13:56:03 -0600

    remove unused boolean from config.

--- src/main/java/xyz/wagyourtail/jsmacros/config/ConfigOptions.java
+++ src/main/java/xyz/wagyourtail/jsmacros/config/ConfigOptions.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ConfigOptions {
-    public boolean allowPrivateAccess;
     public String defaultProfile;
     public HashMap<String, ArrayList<RawMacro>> profiles;
     
