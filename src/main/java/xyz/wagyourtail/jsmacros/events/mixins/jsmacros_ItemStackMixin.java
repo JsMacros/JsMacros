f96de0702816cffc35c2c35481570dd75041245c
wagyourtail <ruwagray@gmail.com>
2020-07-07 15:02:04 -0600

    hopefully a fix for issue #1

--- src/main/java/xyz/wagyourtail/jsmacros/events/mixins/jsmacros_ItemStackMixin.java
+++ src/main/java/xyz/wagyourtail/jsmacros/events/mixins/jsmacros_ItemStackMixin.java
@@ -5,19 +5,16 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.minecraft.client.MinecraftClient;
+import net.minecraft.advancement.criterion.ItemDurabilityChangedCriterion;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
 import xyz.wagyourtail.jsmacros.events.ItemDamageCallback;
 
-@Mixin(ItemStack.class)
-abstract class jsmacros_ItemStackMixin {
+@Mixin(ItemDurabilityChangedCriterion.class)
+abstract class jsmacros_ItemDurabilityChangedCriterionMixin {
     
-    @Inject(at = @At("HEAD"), method="setDamage")
-    private void jsmacros_setDamage(int damage, CallbackInfo info) {
-        MinecraftClient mc = MinecraftClient.getInstance();
-        if (mc != null && mc.player != null && mc.player.inventory != null)
-            if (damage != 0 && mc.player.inventory.contains((ItemStack) (Object) this) ) {
-                ItemDamageCallback.EVENT.invoker().interact((ItemStack) (Object) this, damage);
-            }
+    @Inject(at = @At("HEAD"), method="trigger")
+    private void jsmacros_trigger(ServerPlayerEntity player, ItemStack stack, int damage, CallbackInfo info) {
+        ItemDamageCallback.EVENT.invoker().interact(stack, damage);
     }
 }
