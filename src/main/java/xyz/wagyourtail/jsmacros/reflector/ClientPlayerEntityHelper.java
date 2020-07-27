a78a6d4902c8e6e2cf8cc933c54dab7c85277a84
wagyourtail <ruwagray@gmail.com>
2020-07-27 03:46:36 -0600

    cleanup.

--- src/main/java/xyz/wagyourtail/jsmacros/reflector/ClientPlayerEntityHelper.java
+++ src/main/java/xyz/wagyourtail/jsmacros/reflector/ClientPlayerEntityHelper.java
@@ -2,7 +2,6 @@ package xyz.wagyourtail.jsmacros.reflector;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.MathHelper;
 
 public class ClientPlayerEntityHelper extends PlayerEntityHelper {
