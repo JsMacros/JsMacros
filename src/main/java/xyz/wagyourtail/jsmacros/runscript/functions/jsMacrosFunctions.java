a5363838e3dc7d30b259db5a60ba01f775acc7ab
wagyourtail <ruwagray@gmail.com>
2020-08-02 14:15:17 -0600

    cleanup

--- src/main/java/xyz/wagyourtail/jsmacros/runscript/functions/jsMacrosFunctions.java
+++ src/main/java/xyz/wagyourtail/jsmacros/runscript/functions/jsMacrosFunctions.java
@@ -12,7 +12,6 @@ import net.minecraft.client.gui.screen.ConnectScreen;
 import net.minecraft.client.gui.screen.SaveLevelScreen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
-import net.minecraft.client.network.ServerInfo;
 import net.minecraft.network.ServerAddress;
 import net.minecraft.realms.RealmsBridge;
 import net.minecraft.text.TranslatableText;
