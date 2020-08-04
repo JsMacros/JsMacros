9e4f22d9c0504e55b3463fb2cfd28aae988217c5
wagyourtail <ruwagray@gmail.com>
2020-08-04 12:08:16 -0600

    more reflection stuff... to make JEP happy

--- src/main/java/xyz/wagyourtail/jsmacros/runscript/functions/reflectionFunctions.java
+++ src/main/java/xyz/wagyourtail/jsmacros/runscript/functions/reflectionFunctions.java
@@ -1,6 +1,7 @@
 package xyz.wagyourtail.jsmacros.runscript.functions;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class reflectionFunctions extends Functions {
@@ -11,7 +12,28 @@ public class reflectionFunctions extends Functions {
     }
     
     public Class<?> getClass(String name) throws ClassNotFoundException {
-        return Class.forName(name);
+        switch (name) {
+            case "boolean":
+                return boolean.class;
+            case "byte":
+                return byte.class;
+            case "short":
+                return short.class;
+            case "int":
+                return int.class;
+            case "long":
+                return long.class;
+            case "float":
+                return float.class;
+            case "double":
+                return double.class;
+            case "char":
+                return char.class;
+            case "void":
+                return void.class;
+            default:
+                return Class.forName(name);
+        }
     }
     
     public Class<?> getClass(String name, String name2) throws ClassNotFoundException {
@@ -45,7 +67,17 @@ public class reflectionFunctions extends Functions {
             return c.getDeclaredField(name2);
         }
     }
-
     
-    
+    public Object invokeMethod(Method m, Object c, Object...objects) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        Class<?>[] params = m.getParameterTypes();
+        for (int i = 0; i < objects.length; ++i) {
+            if (params[i] == int.class) {
+                objects[i] = (int)(long)objects[i];
+            } else if (params[i] == float.class) {
+                objects[i] = (float)(double)objects[i];
+            }
+            else objects[i] = params[i].cast(objects[i]);
+        }
+        return m.invoke(c, objects);
+    }
 }
