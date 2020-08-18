34e7f11ae962e6b5340c858218ad939b1cced0e0
wagyourtail <ruwagray@gmail.com>
2020-08-18 08:32:53 -0600

    modmenu support

---
+++ src/main/java/xyz/wagyourtail/jsmacros/ModMenuEntry.java
@@ -0,0 +1,22 @@
+package xyz.wagyourtail.jsmacros;
+
+import io.github.prospector.modmenu.api.ConfigScreenFactory;
+import io.github.prospector.modmenu.api.ModMenuApi;
+import net.minecraft.client.gui.screen.Screen;
+import xyz.wagyourtail.jsmacros.gui2.KeyMacrosScreen;
+import xyz.wagyourtail.jsmacros.gui2.MacroScreen;
+
+public class ModMenuEntry implements ModMenuApi {
+    private JsMacroScreen jsmacrosscreenfactory = new JsMacroScreen();
+    
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return jsmacrosscreenfactory;
+    }
+    
+    public class JsMacroScreen implements ConfigScreenFactory<MacroScreen> {
+        @Override
+        public MacroScreen create(Screen parent) {
+            return new KeyMacrosScreen(parent);
+        }
+    }
+}
