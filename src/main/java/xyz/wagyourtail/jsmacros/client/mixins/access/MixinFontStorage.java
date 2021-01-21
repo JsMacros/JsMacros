6dfb8a69d91fd6fcc9a9eaaed0fb7bf2564a0a73
wagyourtail <wagyourtail@wagyourtail.xyz>
2021-01-20 21:06:02 -0700

    proper mixin implementation fixing ttf space size

--- src/main/java/xyz/wagyourtail/jsmacros/client/mixins/access/MixinFontStorage.java
+++ src/main/java/xyz/wagyourtail/jsmacros/client/mixins/access/MixinFontStorage.java
@@ -1,117 +1,68 @@
 package xyz.wagyourtail.jsmacros.client.mixins.access;
 
-import com.google.common.collect.Sets;
-import it.unimi.dsi.fastutil.ints.*;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.ints.IntList;
 import net.minecraft.client.font.*;
 import net.minecraft.util.math.MathHelper;
-import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
-import java.util.function.IntConsumer;
-import java.util.stream.Stream;
+import java.util.function.IntFunction;
 
 @Mixin(FontStorage.class)
 public abstract class MixinFontStorage {
     
     @Shadow @Final private static Glyph SPACE;
     
-    @Shadow @Final private List<Font> fonts;
-    
     @Shadow @Final private Int2ObjectMap<IntList> charactersByWidth;
     
-    @Shadow protected abstract void closeFonts();
+    @Shadow protected abstract RenderableGlyph getRenderableGlyph(int i);
     
-    @Shadow protected abstract void closeGlyphAtlases();
-    
-    @Shadow @Final private Int2ObjectMap<GlyphRenderer> glyphRendererCache;
-    @Shadow @Final private Int2ObjectMap<Glyph> glyphCache;
-    @Shadow private GlyphRenderer blankGlyphRenderer;
-    
-    
-    @Shadow private GlyphRenderer whiteRectangleGlyphRenderer;
-    
-    @Shadow protected abstract GlyphRenderer getGlyphRenderer(RenderableGlyph c);
-    
-    @Shadow @Final private static EmptyGlyphRenderer EMPTY_GLYPH_RENDERER;
-    
-    /**
-     * @author Wagyourtail
-     */
-    @Overwrite
-    public void setFonts(List<Font> fonts) {
-        this.closeFonts();
-        this.closeGlyphAtlases();
-        this.glyphRendererCache.clear();
-        this.glyphCache.clear();
-        this.charactersByWidth.clear();
-        this.blankGlyphRenderer = this.getGlyphRenderer(BlankGlyph.INSTANCE);
-        this.whiteRectangleGlyphRenderer = this.getGlyphRenderer(WhiteRectangleGlyph.INSTANCE);
-        IntSet intSet = new IntOpenHashSet();
-    
-        for (Font font : fonts) {
-            intSet.addAll(font.getProvidedGlyphs());
-        }
-    
-        Set<Font> set = Sets.newHashSet();
-        intSet.forEach((IntConsumer) (i) -> {
-    
-            for (Font font : fonts) {
-                Glyph glyph = i == 32 && !(font instanceof TrueTypeFont) ? SPACE : font.getGlyph(i);
-                if (glyph != null) {
-                    set.add(font);
-                    if (glyph != BlankGlyph.INSTANCE) {
-                        (this.charactersByWidth.computeIfAbsent(MathHelper.ceil((glyph).getAdvance(false)), (ix) -> new IntArrayList())).add(i);
-                    }
-                    break;
+    // compute proper width of ttf space
+    @Inject(method = "setFonts", at = @At("TAIL"))
+    private void fixSpaceCharWidth(List<Font> fonts, CallbackInfo ci) {
+        if (fonts.size() == 0) return;
+        for (Font f : fonts) {
+            if (f instanceof TrueTypeFont) {
+                Glyph space = f.getGlyph(32);
+                if (space != null) {
+                    charactersByWidth.getOrDefault(MathHelper.ceil(SPACE.getAdvance(false)), new IntArrayList()).rem(32);
+                    this.charactersByWidth.computeIfAbsent(MathHelper.ceil(space.getAdvance(false)), (ix) -> new IntArrayList()).add(32);
+                    return;
                 }
+            } else {
+                return;
             }
-        
-        });
-        Stream<Font> var10000 = fonts.stream();
-        var10000 = var10000.filter(set::contains);
-        List<Font> var10001 = this.fonts;
-        var10000.forEach(var10001::add);
+        }
     }
     
-    /**
-     * @author Wagyourtail
-     */
-    @Overwrite
-    public Glyph getGlyph(int i) {
-        return this.glyphCache.computeIfAbsent(i, (ix) -> {
+    // allow ttf space to be loaded
+    @ModifyArg(method = "getGlyph", at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/ints/Int2ObjectMap;computeIfAbsent(ILjava/util/function/IntFunction;)Ljava/lang/Object;", remap = false))
+    private IntFunction<Glyph> modifyLambda(IntFunction<Glyph> original) {
+        return (ix) -> {
             Glyph g = this.getRenderableGlyph(ix);
-            if (g == null) return SPACE;
+            // null check is for below inject which makes get return null if it hits non ttf font on space char
+            if (ix == 32 && g == null) return SPACE;
             return g;
-        });
+        };
     }
     
-    /**
-     * @author Wagyourtail
-     */
-    @Overwrite
-    private RenderableGlyph getRenderableGlyph(int i) {
-        Iterator<Font> var2 = this.fonts.iterator();
-        
-        RenderableGlyph renderableGlyph;
-        do {
-            if (!var2.hasNext()) {
-                return BlankGlyph.INSTANCE;
-            }
-            Font font = var2.next();
-            if (i == 32 && !(font instanceof TrueTypeFont)) return null;
-            renderableGlyph = font.getGlyph(i);
-        } while(renderableGlyph == null);
-        
-        return renderableGlyph;
-    }
-    
-    /**
-     * @author Wagyourtail
-     */
-    @Overwrite
-    public GlyphRenderer getGlyphRenderer(int i) {
-        return this.glyphRendererCache.computeIfAbsent(i, (ix) -> ix == 32 ? EMPTY_GLYPH_RENDERER : this.getGlyphRenderer(this.getRenderableGlyph(ix)));
+    // return null if space and not TTF, see above method
+    @Inject(method = "getRenderableGlyph", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/Font;getGlyph(I)Lnet/minecraft/client/font/RenderableGlyph;", ordinal = 0), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+    private void nonTTFSpaceSizeFix(int i, CallbackInfoReturnable<RenderableGlyph> cir, Iterator<Font> var2, Font font) {
+        if (i == 32 && !(font instanceof TrueTypeFont)) {
+            cir.setReturnValue(null);
+            cir.cancel();
+        }
     }
 }
--- src/main/java/xyz/wagyourtail/jsmacros/client/mixins/access/MixinTrueTypeFont.java
+++ src/main/java/xyz/wagyourtail/jsmacros/client/mixins/access/MixinTrueTypeFont.java
@@ -8,15 +8,19 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 @Mixin(TrueTypeFont.class)
 public class MixinTrueTypeFont {
     
+    // cancel canceling spaces by making their calculated width/height not equal to 0 which cancels their existence in the font entirely
+    // also I dont actually know which one is width and height, it doesn't matter
+    
+    
     @ModifyVariable(method = "getGlyph", at = @At("STORE"), ordinal = 2)
-    public int modifyWidth(int w) {
-        if (w == 0) return 1;
+    public int modifyWidth(int w, int i) {
+        if (i == 32) return 1;
         return w;
     }
     
     @ModifyVariable(method = "getGlyph", at = @At("STORE"), ordinal = 3)
-    public int modifyHeight(int h) {
-        if (h == 0) return 1;
+    public int modifyHeight(int h, int i) {
+        if (i == 32) return 1;
         return h;
     }
     
