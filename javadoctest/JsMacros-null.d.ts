bd5425cfb726765cc4c3e1d363075eec4ac07d67
wagyourtail <wagyourtail@wagyourtail.xyz>
2021-06-02 18:23:01 -0700

    Delete javadoctest directory

--- javadoctest/JsMacros-null.d.ts
+++
@@ -1,8 +0,0 @@
-declare const event: Events.BaseEvent;
-declare const file: Java.java.io.File;
-declare const context: Java.xyz.wagyourtail.jsmacros.core.language.ContextContainer<any>;
-
-declare namespace Events {
-    export interface BaseEvent extends Java.Object {
-        getEventName(): string;
-    }
