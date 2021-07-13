3d9c1b6695278669ca3a6f85a5e28a31480bb92a
Joe <burtonjae@hotmail.co.uk>
2021-07-13 11:19:01 +0100

    Add resource reloaded event

---
+++ src/main/java/xyz/wagyourtail/jsmacros/client/mixins/events/MixinSplashOverlay.java
@@ -0,0 +1,21 @@
+package xyz.wagyourtail.jsmacros.client.mixins.events;
+
+import net.minecraft.client.gui.screen.SplashOverlay;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import xyz.wagyourtail.jsmacros.client.api.event.impl.EventResourcesReloaded;
+
+@Mixin(SplashOverlay.class)
+public class MixinSplashOverlay {
+    @Shadow @Final private boolean reloading;
+
+    @Inject(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/screen/SplashOverlay;reloadCompleteTime:J", opcode = Opcodes.PUTFIELD))
+    private void onReloadComplete(CallbackInfo ci) {
+        new EventResourcesReloaded(!reloading);
+    }
+}
