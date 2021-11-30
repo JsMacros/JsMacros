e0fa50f38a2faa415df3cd9d2cf6a18a8a8e1c63
Wagyourtail <wagyourtail@wagyourtail.xyz>
2021-11-30 07:29:27 -0700

    yeet unused code

--- common/src/main/java/xyz/wagyourtail/jsmacros/client/mixins/access/MixinEntity.java
+++ common/src/main/java/xyz/wagyourtail/jsmacros/client/mixins/access/MixinEntity.java
@@ -2,7 +2,6 @@ package xyz.wagyourtail.jsmacros.client.mixins.access;
 
 import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,10 +16,6 @@ public abstract class MixinEntity implements IMixinEntity {
 
     @Unique
     private int forceGlowing = 1;
-
-
-    @Shadow
-    public abstract boolean isGlowingLocal();
 
     @Override
     public void jsmacros_setGlowingColor(int glowingColor) {
