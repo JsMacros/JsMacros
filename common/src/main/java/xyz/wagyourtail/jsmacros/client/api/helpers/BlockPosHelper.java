8851236a1d35cbbad7ed4eab7690639db7457afd
Etheradon <91631266+Etheradon@users.noreply.github.com>
2022-01-23 11:45:52 +0100

    added some javadoc

--- common/src/main/java/xyz/wagyourtail/jsmacros/client/api/helpers/BlockPosHelper.java
+++ common/src/main/java/xyz/wagyourtail/jsmacros/client/api/helpers/BlockPosHelper.java
@@ -2,7 +2,6 @@ package xyz.wagyourtail.jsmacros.client.api.helpers;
 
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.Vec3i;
 import xyz.wagyourtail.jsmacros.core.helpers.BaseHelper;
 
 /**
@@ -45,69 +44,136 @@ public class BlockPosHelper extends BaseHelper<BlockPos> {
     }
 
     /**
-     * @since 1.6.5
      * @return the block above.
+     *
+     * @since 1.6.5
      */
     public BlockPosHelper up() {
         return new BlockPosHelper(getX(), getY() + 1, getZ());
     }
 
     /**
+     * @param distance
+     * @return the block n-th block above.
+     *
      * @since 1.6.5
-     * @return the block above.
      */
     public BlockPosHelper up(int distance) {
         return new BlockPosHelper(getX(), getY() + distance, getZ());
     }
 
     /**
+     * @return the block below.
+     *
      * @since 1.6.5
-     * @return the block above.
      */
     public BlockPosHelper down() {
         return new BlockPosHelper(getX(), getY() - 1, getZ());
     }
 
+    /**
+     * @param distance
+     * @return the block n-th block below.
+     *
+     * @since 1.6.5
+     */
     public BlockPosHelper down(int distance) {
         return new BlockPosHelper(getX(), getY() - distance, getZ());
     }
 
+    /**
+     * @return the block to the north.
+     *
+     * @since 1.6.5
+     */
     public BlockPosHelper north() {
         return new BlockPosHelper(getX(), getY(), getZ() - 1);
     }
 
+    /**
+     * @param distance
+     * @return the n-th block to the north.
+     *
+     * @since 1.6.5
+     */
     public BlockPosHelper north(int distance) {
         return new BlockPosHelper(getX(), getY(), getZ() - distance);
     }
 
+    /**
+     * @return the block to the south.
+     *
+     * @since 1.6.5
+     */
     public BlockPosHelper south() {
         return new BlockPosHelper(getX(), getY(), getZ() + 1);
     }
 
+    /**
+     * @param distance
+     * @return the n-th block to the south.
+     *
+     * @since 1.6.5
+     */
     public BlockPosHelper south(int distance) {
         return new BlockPosHelper(getX(), getY(), getZ() + distance);
     }
 
+    /**
+     * @return the block to the east.
+     *
+     * @since 1.6.5
+     */
     public BlockPosHelper east() {
         return new BlockPosHelper(getX() + 1, getY(), getZ());
     }
 
+    /**
+     * @param distance
+     * @return the n-th block to the east.
+     *
+     * @since 1.6.5
+     */
     public BlockPosHelper east(int distance) {
         return new BlockPosHelper(getX() + distance, getY(), getZ());
     }
 
+    /**
+     * @return the block to the west.
+     *
+     * @since 1.6.5
+     */
     public BlockPosHelper west() {
         return new BlockPosHelper(getX() - 1, getY(), getZ());
     }
 
+    /**
+     * @param distance
+     * @return the n-th block to the west.
+     *
+     * @since 1.6.5
+     */
     public BlockPosHelper west(int distance) {
         return new BlockPosHelper(getX() - distance, getY(), getZ());
     }
-    
+
+    /**
+     * @param direction
+     * @return the block offset by the given direction.
+     *
+     * @since 1.6.5
+     */
     public BlockPosHelper offset(String direction) {
         return new BlockPosHelper(base.offset(Direction.byName(direction)));
     }
 
+    /**
+     * @param direction
+     * @param distance
+     * @return the n-th block offset by the given direction.
+     *
+     * @since 1.6.5
+     */
     public BlockPosHelper offset(String direction, int distance) {
         return new BlockPosHelper(base.offset(Direction.byName(direction)));
     }
--- common/src/main/java/xyz/wagyourtail/jsmacros/client/api/helpers/BlockStateHelper.java
+++ common/src/main/java/xyz/wagyourtail/jsmacros/client/api/helpers/BlockStateHelper.java
@@ -3,13 +3,11 @@ package xyz.wagyourtail.jsmacros.client.api.helpers;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.ai.pathing.NavigationType;
-import net.minecraft.state.property.Property;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Util;
 import net.minecraft.util.registry.Registry;
 import xyz.wagyourtail.jsmacros.core.helpers.BaseHelper;
 
-import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -24,57 +22,119 @@ public class BlockStateHelper extends BaseHelper<BlockState> {
         super(base);
     }
 
+    /**
+     * @return a map of the state properties with its identifier and value.
+     *
+     * @version 1.6.5
+     */
     public Map<String, String> toMap() {
-        return base.getEntries().entrySet().stream().collect(Collectors.toMap(
-                entry -> entry.getKey().getName(),
-                entry -> Util.getValueAsString(entry.getKey(), entry.getValue())
-        ));
+        return base.getEntries().entrySet().stream().collect(Collectors.toMap(entry -> entry.getKey().getName(), entry -> Util.getValueAsString(entry.getKey(), entry.getValue())));
     }
 
+    /**
+     * @return the block the state belongs to.
+     *
+     * @version 1.6.5
+     */
     public BlockHelper getBlock() {
         return new BlockHelper(base.getBlock());
     }
 
+    /**
+     * @return the hardness.
+     *
+     * @version 1.6.5
+     */
     public float getHardness() {
         return base.getHardness(null, null);
     }
 
+    /**
+     * @return the luminance.
+     *
+     * @version 1.6.5
+     */
     public int getLuminance() {
         return base.getLuminance();
     }
 
+    /**
+     * @return {@code true} if the state emits redstone power.
+     *
+     * @version 1.6.5
+     */
     public boolean emitsRedstonePower() {
         return base.emitsRedstonePower();
     }
 
+    /**
+     * @return {@code true} if the shape of the state is a cube.
+     *
+     * @version 1.6.5
+     */
     public boolean exceedsCube() {
         return base.exceedsCube();
     }
 
+    /**
+     * @return {@code true} if the state is air.
+     *
+     * @version 1.6.5
+     */
     public boolean isAir() {
         return base.isAir();
     }
 
+    /**
+     * @return {@code true} if the state is opaque.
+     *
+     * @version 1.6.5
+     */
     public boolean isOpaque() {
         return base.isOpaque();
     }
 
+    /**
+     * @return {@code true} if a tool is required to mine the block.
+     *
+     * @version 1.6.5
+     */
     public boolean isToolRequired() {
         return base.isToolRequired();
     }
 
+    /**
+     * @return {@code true} if the state has a block entity.
+     *
+     * @version 1.6.5
+     */
     public boolean hasBlockEntity() {
         return base.hasBlockEntity();
     }
 
+    /**
+     * @return {@code true} if the state can be random ticked.
+     *
+     * @version 1.6.5
+     */
     public boolean hasRandomTicks() {
         return base.hasRandomTicks();
     }
 
+    /**
+     * @return {@code true} if the state has a comparator output.
+     *
+     * @version 1.6.5
+     */
     public boolean hasComparatorOutput() {
         return base.hasComparatorOutput();
     }
 
+    /**
+     * @return the piston behaviour of the state.
+     *
+     * @version 1.6.5
+     */
     public String getPistonBehaviour() {
         System.out.println(this + " " + base.getPistonBehavior());
         switch (base.getPistonBehavior()) {
@@ -93,44 +153,82 @@ public class BlockStateHelper extends BaseHelper<BlockState> {
         }
     }
 
-    public boolean isInstaMineable() {
-        return getHardness() <= 0;
-    }
-    
+    /**
+     * @return {@code true} if the state blocks light.
+     *
+     * @version 1.6.5
+     */
     public boolean blocksLight() {
         return base.getMaterial().blocksLight();
     }
 
+    /**
+     * @return {@code true} if the state blocks the movement of entities.
+     *
+     * @version 1.6.5
+     */
     public boolean blocksMovement() {
         return base.getMaterial().blocksMovement();
     }
 
+    /**
+     * @return {@code true} if the state is burnable.
+     *
+     * @version 1.6.5
+     */
     public boolean isBurnable() {
         return base.getMaterial().isBurnable();
     }
 
+    /**
+     * @return {@code true} if the state is a liquid.
+     *
+     * @version 1.6.5* @version 1.6.5
+     */
     public boolean isLiquid() {
         return base.getMaterial().isLiquid();
     }
 
+    /**
+     * @return {@code true} if the state is solid.
+     *
+     * @version 1.6.5* @version 1.6.5
+     */
     public boolean isSolid() {
         return base.getMaterial().isSolid();
     }
 
+    /**
+     * This will return true for blocks like air and grass, that can be replaced
+     * without breaking them first.
+     *
+     * @return {@code true} if the state can be replaced.
+     *
+     * @version 1.6.5
+     */
     public boolean isReplaceable() {
         return base.getMaterial().isReplaceable();
     }
 
+    /**
+     * @param pos
+     * @param entity
+     * @return {@code true} if the entity can spawn on this block state at the given position in the current world.
+     *
+     * @version 1.6.5
+     */
     public boolean allowsSpawning(BlockPosHelper pos, String entity) {
         return base.allowsSpawning(MinecraftClient.getInstance().world, pos.getRaw(), Registry.ENTITY_TYPE.get(new Identifier(entity)));
     }
 
+    /**
+     * @param pos
+     * @return {@code true} if an entity can suffocate in this block state at the given position in the current world.
+     *
+     * @version 1.6.5
+     */
     public boolean shouldSuffocate(BlockPosHelper pos) {
         return base.shouldSuffocate(MinecraftClient.getInstance().world, pos.getRaw());
-    }
-
-    public boolean canPathfindThrough(BlockPosHelper pos, String navigationType) {
-        return base.canPathfindThrough(MinecraftClient.getInstance().world, pos.getRaw(), getNavigationType(navigationType));
     }
 
     private static NavigationType getNavigationType(String navigationType) {
