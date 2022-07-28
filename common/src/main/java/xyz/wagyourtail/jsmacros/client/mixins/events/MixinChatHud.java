baa8efa317a584fa85276e663584c59b38c138a1
Wagyourtail <wagyourtail@wagyourtail.xyz>
2022-07-28 15:28:52 -0600

    fix chat recieve event

--- common/src/main/java/xyz/wagyourtail/jsmacros/client/mixins/events/MixinChatHud.java
+++ common/src/main/java/xyz/wagyourtail/jsmacros/client/mixins/events/MixinChatHud.java
@@ -1,8 +1,11 @@
 package xyz.wagyourtail.jsmacros.client.mixins.events;
 
 import net.minecraft.client.gui.hud.ChatHud;
+import net.minecraft.client.gui.hud.MessageIndicator;
+import net.minecraft.network.message.MessageSignatureData;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -10,13 +13,29 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.wagyourtail.jsmacros.client.api.event.impl.EventRecvMessage;
 import xyz.wagyourtail.jsmacros.client.api.helpers.TextHelper;
 
+import java.util.concurrent.Semaphore;
+
 @Mixin(ChatHud.class)
 class MixinChatHud {
+    @Unique
+    private EventRecvMessage eventRecvMessage;
 
-    @ModifyVariable(method = "addMessage(Lnet/minecraft/text/Text;)V", at = @At(value = "HEAD"))
+    @Inject(
+        method = "addMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/message/MessageSignatureData;Lnet/minecraft/client/gui/hud/MessageIndicator;)V",
+        at = @At("HEAD")
+    )
+    private void onAddMessage1(Text message, MessageSignatureData signature, MessageIndicator indicator, CallbackInfo ci) {
+        eventRecvMessage = new EventRecvMessage(message, signature, indicator);
+    }
+
+    @ModifyVariable(
+        method = "addMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/message/MessageSignatureData;Lnet/minecraft/client/gui/hud/MessageIndicator;)V",
+        at = @At(value = "HEAD"),
+        argsOnly = true
+    )
     private Text modifyChatMessage(Text text) {
-        if (text == null) return text;
-        final TextHelper result = new EventRecvMessage(text).text;
+        if (text == null) return null;
+        final TextHelper result = eventRecvMessage.text;
         if (result == null) return null;
         if (!result.getRaw().equals(text)) {
             return result.getRaw();
@@ -24,10 +43,14 @@ class MixinChatHud {
         else return text;
     }
 
-    @Inject(method = "addMessage(Lnet/minecraft/text/Text;)V", at = @At("HEAD"), cancellable = true)
-    private void onAddChatMessage(Text text, CallbackInfo info) {
-        if (text == null) {
-            info.cancel();
+    @Inject(
+        method = "addMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/message/MessageSignatureData;Lnet/minecraft/client/gui/hud/MessageIndicator;)V",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private void onAddChatMessage(Text message, MessageSignatureData signature, MessageIndicator indicator, CallbackInfo ci) {
+        if (message == null) {
+            ci.cancel();
         }
     }
 }
