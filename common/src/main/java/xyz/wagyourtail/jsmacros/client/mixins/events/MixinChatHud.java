fec18029a729bae8b00f045427e5912ca1a385ab
Wagyourtail <wagyourtail@wagyourtail.xyz>
2022-07-28 16:02:26 -0600

    jsmacros text modified on 1.19.1

--- common/src/main/java/xyz/wagyourtail/jsmacros/client/mixins/events/MixinChatHud.java
+++ common/src/main/java/xyz/wagyourtail/jsmacros/client/mixins/events/MixinChatHud.java
@@ -3,7 +3,10 @@ package xyz.wagyourtail.jsmacros.client.mixins.events;
 import net.minecraft.client.gui.hud.ChatHud;
 import net.minecraft.client.gui.hud.MessageIndicator;
 import net.minecraft.network.message.MessageSignatureData;
+import net.minecraft.screen.ScreenTexts;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,14 +22,20 @@ import java.util.concurrent.Semaphore;
 class MixinChatHud {
     @Unique
     private EventRecvMessage eventRecvMessage;
+    @Unique
+    private Text originalMessage;
 
     @Inject(
         method = "addMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/message/MessageSignatureData;Lnet/minecraft/client/gui/hud/MessageIndicator;)V",
         at = @At("HEAD")
     )
     private void onAddMessage1(Text message, MessageSignatureData signature, MessageIndicator indicator, CallbackInfo ci) {
+        originalMessage = message;
         eventRecvMessage = new EventRecvMessage(message, signature, indicator);
     }
+
+    @Unique
+    private boolean modifiedEventRecieve;
 
     @ModifyVariable(
         method = "addMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/message/MessageSignatureData;Lnet/minecraft/client/gui/hud/MessageIndicator;)V",
@@ -34,13 +43,38 @@ class MixinChatHud {
         argsOnly = true
     )
     private Text modifyChatMessage(Text text) {
+        modifiedEventRecieve = false;
         if (text == null) return null;
         final TextHelper result = eventRecvMessage.text;
         if (result == null) return null;
         if (!result.getRaw().equals(text)) {
+            modifiedEventRecieve = true;
             return result.getRaw();
+        } else {
+            return text;
         }
-        else return text;
+    }
+
+    @Unique
+    private final Text MODIFIED_TEXT = Text.translatable("jsmacros.chat.tag.modified").formatted(Formatting.UNDERLINE);
+
+    @ModifyVariable(
+        method = "addMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/message/MessageSignatureData;Lnet/minecraft/client/gui/hud/MessageIndicator;)V",
+        at = @At(value = "HEAD"),
+        argsOnly = true
+    )
+    private MessageIndicator modifyChatMessageSignature(MessageIndicator signature) {
+        if (modifiedEventRecieve) {
+            MutableText text2 = Text.empty().append(MODIFIED_TEXT).append(ScreenTexts.LINE_BREAK);
+            if (signature != null && signature.text() != null) {
+                text2.append(originalMessage).append(ScreenTexts.LINE_BREAK).append(signature.text());
+            } else {
+                text2.append(originalMessage);
+            }
+            return new MessageIndicator(15386724, MessageIndicator.Icon.CHAT_MODIFIED, text2, "Modified");
+        } else {
+            return signature;
+        }
     }
 
     @Inject(
