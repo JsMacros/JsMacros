31993caefb2eedbb72e0c114c5be5c9a4e9b0631
Wagyourtail <wagyourtail@wagyourtail.xyz>
2023-07-15 08:55:28 -0600

    make game message actionbar trigger title event

---
+++ src/main/java/xyz/wagyourtail/jsmacros/client/mixins/events/MixinMessageHandler.java
@@ -0,0 +1,23 @@
+package xyz.wagyourtail.jsmacros.client.mixins.events;
+
+import net.minecraft.client.network.message.MessageHandler;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import xyz.wagyourtail.jsmacros.client.api.event.impl.EventTitle;
+
+@Mixin(MessageHandler.class)
+public class MixinMessageHandler {
+
+    @ModifyArg(method = "onGameMessage", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;setOverlayMessage(Lnet/minecraft/text/Text;Z)V"))
+    private Text modifyOverlayMessage(Text text) {
+        EventTitle et = new EventTitle("ACTIONBAR", text);
+        if (et.message == null) {
+            return null;
+        } else {
+            return et.message.getRaw();
+        }
+    }
+
+}
