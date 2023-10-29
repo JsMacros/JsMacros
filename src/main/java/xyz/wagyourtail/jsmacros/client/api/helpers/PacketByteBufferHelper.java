25d1b07ebc85107c40d1c25212954076921ccaba
aMelonRind <kong49123@gmail.com>
2023-10-29 11:18:46 +0800

    change BlockHitResult related methods in PacketByteBufferHelper

--- src/main/java/xyz/wagyourtail/jsmacros/client/api/helpers/PacketByteBufferHelper.java
+++ src/main/java/xyz/wagyourtail/jsmacros/client/api/helpers/PacketByteBufferHelper.java
@@ -43,6 +43,7 @@ import xyz.wagyourtail.jsmacros.client.api.helpers.inventory.ItemStackHelper;
 import xyz.wagyourtail.jsmacros.client.api.helpers.world.BlockPosHelper;
 import xyz.wagyourtail.jsmacros.client.api.helpers.world.ChunkHelper;
 import xyz.wagyourtail.jsmacros.client.api.helpers.world.DirectionHelper;
+import xyz.wagyourtail.jsmacros.client.api.helpers.world.HitResultHelper;
 import xyz.wagyourtail.jsmacros.core.MethodWrapper;
 import xyz.wagyourtail.jsmacros.core.helpers.BaseHelper;
 
@@ -988,8 +989,19 @@ public class PacketByteBufferHelper extends BaseHelper<PacketByteBuf> {
      * @return self for chaining.
      * @since 1.8.4
      */
+    @Deprecated
     public PacketByteBufferHelper writeBlockHitResult(BlockHitResult hitResult) {
         base.writeBlockHitResult(hitResult);
+        return this;
+    }
+
+    /**
+     * @param hitResult the hit result to store
+     * @return self for chaining.
+     * @since 1.9.0
+     */
+    public PacketByteBufferHelper writeBlockHitResult(HitResultHelper.Block hitResult) {
+        base.writeBlockHitResult((BlockHitResult) hitResult.getRaw());
         return this;
     }
 
@@ -1009,13 +1021,15 @@ public class PacketByteBufferHelper extends BaseHelper<PacketByteBuf> {
         } else {
             result = new BlockHitResult(pos.toMojangDoubleVector(), Direction.valueOf(direction), blockPos.getRaw(), insideBlock);
         }
-        return writeBlockHitResult(result);
+        base.writeBlockHitResult(result);
+        return this;
     }
 
     /**
      * @return the read block hit result.
      * @since 1.8.4
      */
+    @Deprecated
     public BlockHitResult readBlockHitResult() {
         return base.readBlockHitResult();
     }
@@ -1024,9 +1038,18 @@ public class PacketByteBufferHelper extends BaseHelper<PacketByteBuf> {
      * @return a map of the block hit result's data and their values.
      * @since 1.8.4
      */
+    @Deprecated
     public Map<String, Object> readBlockHitResultMap() {
-        BlockHitResult hitResult = readBlockHitResult();
+        BlockHitResult hitResult = base.readBlockHitResult();
         return ImmutableMap.of("side", new DirectionHelper(hitResult.getSide()), "blockPos", new BlockPosHelper(hitResult.getBlockPos()), "missed", hitResult.getType() == HitResult.Type.MISS, "inside", hitResult.isInsideBlock());
+    }
+
+    /**
+     * @return the read block hit result as a helper.
+     * @since 1.9.0
+     */
+    public HitResultHelper.Block readBlockHitResultHelper() {
+        return new HitResultHelper.Block(base.readBlockHitResult());
     }
 
     /**
