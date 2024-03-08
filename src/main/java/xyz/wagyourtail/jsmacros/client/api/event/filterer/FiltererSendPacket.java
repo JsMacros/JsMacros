9793f577cbdc67f4549b31b2e7a85d4d1cd1fc72
aMelonRind <kong49123@gmail.com>
2024-03-08 18:11:53 +0800

    add event filterers for RecvPacket and SendPacket

---
+++ src/main/java/xyz/wagyourtail/jsmacros/client/api/event/filterer/FiltererSendPacket.java
@@ -0,0 +1,34 @@
+package xyz.wagyourtail.jsmacros.client.api.event.filterer;
+
+import org.jetbrains.annotations.Nullable;
+import xyz.wagyourtail.doclet.DocletReplaceReturn;
+import xyz.wagyourtail.jsmacros.client.api.event.impl.EventSendPacket;
+import xyz.wagyourtail.jsmacros.core.event.BaseEvent;
+import xyz.wagyourtail.jsmacros.core.event.EventFilterer;
+
+/**
+ * @author aMelonRind
+ * @since 1.9.1
+ */
+@SuppressWarnings("unused")
+public class FiltererSendPacket implements EventFilterer {
+    @DocletReplaceReturn("PacketName | null")
+    @Nullable
+    public String type;
+
+    @Override
+    public boolean canFilter(String event) {
+        return "SendPacket".equals(event);
+    }
+
+    @Override
+    public boolean test(BaseEvent event) {
+        return (event instanceof EventSendPacket e) && (type == null || e.type.equals(type));
+    }
+
+    public FiltererSendPacket setType(String type) {
+        this.type = type;
+        return this;
+    }
+
+}
