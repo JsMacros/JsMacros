547efbea3437bb15446eaf4a1e0d3cf7ed335d18
Wagyourtail <wagyourtail@wagyourtail.xyz>
2024-06-08 16:16:10 -0500

    fix draw3d

--- src/fabric/java/xyz/wagyourtail/jsmacros/fabric/client/mixins/access/MixinGameRenderer.java
+++ src/fabric/java/xyz/wagyourtail/jsmacros/fabric/client/mixins/access/MixinGameRenderer.java
@@ -1,12 +1,14 @@
 package xyz.wagyourtail.jsmacros.fabric.client.mixins.access;
 
 import com.google.common.collect.ImmutableSet;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.util.math.MatrixStack;
+import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -49,15 +51,21 @@ public class MixinGameRenderer {
     }
 
     @Inject(at = @At(value = "INVOKE_STRING", target = "Lnet/minecraft/util/profiler/Profiler;swap(Ljava/lang/String;)V", args = "ldc=hand"), method = "renderWorld")
-    public void render(float tickDelta, long limitTime, CallbackInfo ci) {
+    public void render(float tickDelta, long limitTime, CallbackInfo ci, @Local(ordinal = 1) Matrix4f matrix4f2) {
         client.getProfiler().swap("jsmacros_draw3d");
-        for (Draw3D d : ImmutableSet.copyOf(FHud.renders)) {
-            try {
-                DrawContext drawContext = DRAW_CONTEXT_CONSTRUCTOR.newInstance(client, new MatrixStack(), client.getBufferBuilders().getEntityVertexConsumers());
-                d.render(drawContext, tickDelta);
-            } catch (Throwable e) {
-                e.printStackTrace();
+        MatrixStack ms = new MatrixStack();
+        ms.multiplyPositionMatrix(matrix4f2);
+        try {
+            DrawContext drawContext = DRAW_CONTEXT_CONSTRUCTOR.newInstance(client, ms, client.getBufferBuilders().getEntityVertexConsumers());
+            for (Draw3D d : ImmutableSet.copyOf(FHud.renders)) {
+                try {
+                    d.render(drawContext, tickDelta);
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                }
             }
+        } catch (Throwable e) {
+            e.printStackTrace();
         }
         client.getProfiler().pop();
     }
