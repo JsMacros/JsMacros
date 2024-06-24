0f7c5385239e4414c172869336dd9a47f5d85227
Wagyourtail <wagyourtail@wagyourtail.xyz>
2024-06-24 05:20:50 -0500

    fix build

--- src/fabric/java/xyz/wagyourtail/jsmacros/fabric/client/mixins/access/MixinGameRenderer.java
+++ src/fabric/java/xyz/wagyourtail/jsmacros/fabric/client/mixins/access/MixinGameRenderer.java
@@ -5,7 +5,6 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
-import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
